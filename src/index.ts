c8aae0e8c919836ca4139850dc883f76b6a5e2a5
Guilherme Afonso <gasp@cin.ufpe.br>
2023-08-18 23:13:35 -0300

    feat: add privacy policy

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotevn from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
 import * as userAuth from './routes/userAuth';
+import * as privacyPolicy from './routes/privacyPolicy';
 import { mongoConnection } from './database/mongo';
 import http from 'http';
 import https from 'https';
@@ -41,6 +42,7 @@ app.use(cors(corsOptionsDelegate));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/user-auth', userAuth.default);
+app.use('/privacy-policy', privacyPolicy.default);
 
 mongoConnection().then(() => {
   const httpServer = http.createServer(app);

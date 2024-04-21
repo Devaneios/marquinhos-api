d8db5e804482212765872f12255301b74762a8db
Guilherme Afonso <guilhermeafonso208@gmail.com>
2024-04-20 21:16:51 -0300

    feat: add bypass to web on token verification

--- src/middlewares/botAuth.ts
+++ src/middlewares/botAuth.ts
@@ -1,7 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
+import { verifyDiscordToken } from './userAuth';
 
 export function checkToken(req: Request, res: Response, next: NextFunction) {
   const authorization = req.headers['authorization'] as string;
+  const isWebRequest = req.headers['marquinhos-agent'] === 'web';
+  if (isWebRequest) {
+    return verifyDiscordToken(req, res, next);
+  }
+  console.log(req.headers);
   const token = authorization && authorization.split(' ')[1];
   if (!token) {
     return res.status(401).json({ message: 'Token not provided' });
